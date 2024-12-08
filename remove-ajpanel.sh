6877b42e0d265f1e6584e21ec40d66fb90a2ad74
AMAJamry <164648277+AMAJamry@users.noreply.github.com>
2024-12-08 05:59:12 +0300

    A script to remove AJPanel

---
+++ remove-ajpanel.sh
@@ -0,0 +1,11 @@
+opkg remove enigma2-plugin-extensions-ajpanel &>/dev/null
+opkg remove --force-remove enigma2-plugin-extensions-ajpanel &>/dev/null
+opkg remove --force-depends enigma2-plugin-extensions-ajpanel &>/dev/null
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AJPan &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.list &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.postinst &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.postrm &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.control &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.preinst &>/dev/null
+rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.prerm &>/dev/null
+killall -9 enigma2
