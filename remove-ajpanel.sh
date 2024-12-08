9874e97e14170312a560485e5f2033b085f99c3e
AMAJamry <164648277+AMAJamry@users.noreply.github.com>
2024-12-08 06:09:48 +0300

    Update remove-ajpanel.sh

--- remove-ajpanel.sh
+++ remove-ajpanel.sh
@@ -1,3 +1,4 @@
+echo Removeing AJPanel ...
 opkg remove enigma2-plugin-extensions-ajpanel &>/dev/null
 opkg remove --force-remove enigma2-plugin-extensions-ajpanel &>/dev/null
 opkg remove --force-depends enigma2-plugin-extensions-ajpanel &>/dev/null
@@ -8,4 +9,6 @@ rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.postrm &>/dev/null
 rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.control &>/dev/null
 rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.preinst &>/dev/null
 rm -f /var/lib/opkg/info/enigma2-plugin-extensions-ajpanel.prerm &>/dev/null
+echo Restarting GUI ...
 killall -9 enigma2
+echo Done
